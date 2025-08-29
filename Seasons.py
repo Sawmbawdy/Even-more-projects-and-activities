0043ed54b436540556538b59761dcdc18b1f4e1a
Sawmbawdy <saiompatro12@gmail.com>
2025-08-29 19:32:06 -0400

    Add files via upload

---
+++ Seasons.py
@@ -0,0 +1,7 @@
+def seasons():
+    print("The weather is nice in", Spring)
+    print("The weather is also nice in", Autumn)
+
+Spring = input("Enter the name of the first season: \n")
+Autumn = input("Enter the name of the second season: \n")
+print(seasons())
